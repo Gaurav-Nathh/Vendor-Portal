ee056cf11c7cc1149f128d6e943fd60df05970ca
Gaurav-Nathh <gauravnath725@gmail.com>
2025-04-24 17:58:41 +0530

    Navbar

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,7 +1,8 @@
 import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-import { ThemeToggleComponent } from "../../components/theme-toggle/theme-toggle.component";
+import { ThemeToggleComponent } from '../../components/theme-toggle/theme-toggle.component';
+import { UserService } from '../../services/user.service';
 
 interface NavItem {
   icon: string;
@@ -15,7 +16,9 @@ interface NavItem {
   styleUrls: ['./navbar.component.scss'],
   imports: [ThemeToggleComponent],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  greeting: string = '';
+  userType: string = '';
   navItems: NavItem[] = [
     { icon: 'bi-house', label: 'Home', route: '/home' },
     { icon: 'bi-person', label: 'Profile', route: '/profile' },
@@ -23,7 +26,26 @@ export class NavbarComponent {
     { icon: 'bi-box-arrow-right', label: 'Logout', route: '/logout' },
   ];
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userType = this.userService.getUserType();
+    const hour = new Date().getHours();
+
+    if (hour < 12) {
+      this.greeting = 'Good Morning';
+    } else if (hour < 17) {
+      this.greeting = 'Good Afternoon';
+    } else {
+      this.greeting = 'Good Evening';
+    }
+
+    this.greeting += `, ${this.capitalize(this.userType)}`;
+  }
+
+  capitalize(text: string): string {
+    return text.charAt(0).toUpperCase() + text.slice(1);
+  }
 
   toggleMobileMenu() {
     const mobileMenu = document.getElementById('mobileMenu');
