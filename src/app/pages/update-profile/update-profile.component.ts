5c4472668326ae4c69ec148558755d3a86d33a7e
ayush <panwarkanu2001@gmail.com>
2025-05-03 10:18:59 +0530

    profile

--- src/app/pages/update-profile/update-profile.component.ts
+++ src/app/pages/update-profile/update-profile.component.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-update-profile',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './update-profile.component.html',
   styleUrl: './update-profile.component.scss'
 })
 export class UpdateProfileComponent {
+  enablePortal: boolean = false;
+  enableProfileEditing: boolean = false;
+  enablePurchaseOrderCreation: boolean = false;
+  enableManualInvoiceGeneration: boolean = false;
+  portalUrl: string = '';
 
+  onPortalToggle() {
+    if (!this.enablePortal) {
+      this.portalUrl = '';
+    }
+  }
 }
